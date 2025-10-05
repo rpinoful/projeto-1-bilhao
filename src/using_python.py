22fe22b8b0df92b008399e6fade5fc9bab1f7f7a
Ruben <rapsful@gmail.com>
2025-10-05 12:55:13 -0300

    Corrige lectura de CSV y manejo de filas incompletas

--- src/using_python.py
+++ src/using_python.py
@@ -1,1 +1,20 @@
-print ("hello world")
+from csv import reader
+from pathlib import Path
+
+#configurando path dinamico
+path_do_csv = Path("data") / "weather_stations.csv"
+
+
+#lectura del archivo
+with open(path_do_csv, 'r', encoding='utf-8') as file:
+    _reader = reader(file, delimiter=';')  # Nota: solo reader
+    
+    
+# Percorre el archivo csv almacenada en la variavel e retorna su contenido    
+    for row in _reader:
+        if len(row)<2:
+            continue
+        print(row)
+        nome_station = row[0]
+        temperatura = float(row[1])
+        print(f"Estación: {nome_station}, Temperatura: {temperatura}")
